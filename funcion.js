25f0e3268154304428f956b01206a60f4f039801
Jostin2210 <138197968+Jostin2210@users.noreply.github.com>
2023-06-30 03:13:26 -0500

    initial commit

---
+++ funcion.js
@@ -0,0 +1,6 @@
+function sumar(){
+    var num1=parseFloat(document.getElementById('num1').value);
+    var num2=parseFloat(document.getElementById('num2').value);
+    var calcular= num1+num2;
+    alert("Aqui tienes tu resultado pendejo de mierda: "+calcular);
+}
